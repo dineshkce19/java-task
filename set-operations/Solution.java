f8302e6a1ec048c080a328265950027d79749a2c
Dinesh Raja .N <dineshnkce@gmail.com>
2016-09-02 21:45:44 +0530

    Create Solution.java

---
+++ set-operations/Solution.java
@@ -0,0 +1,11 @@
+package org.creators;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserMenu usr = new UserMenu();
+		usr.select();
+	}
+
+}
